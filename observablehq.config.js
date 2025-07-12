106c0883949c649278de84f27c8a4536f17c3154
stephane-klein <contact@stephane-klein.info>
2025-07-12 18:14:25 +0200

    Add view source in footer

--- observablehq.config.js
+++ observablehq.config.js
@@ -5,5 +5,5 @@ export default {
     theme: "light",
     style: "default.css",
     header: "",
-    footer: ""
+    footer: ({path}) => `<a href="https://github.com/stephane-klein/observable-playground/blob/main/src${path}.md">view source</a>`
 };
